15e28d4cf4c94147bd7703955a9ef487c5ddadd1
Mahfod <mahfod.dev@gmail.com>
2023-01-20 08:13:48 +0100

    create navbar and test listitems

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -6,7 +6,23 @@
       <div
         class="border-blue flex h-full flex-nowrap border-b border-brand-gray-1 px-8"
       >
-        <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
+        <a class="flex h-full items-center text-xl" :href="url">{{
+          company
+        }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              class="ml-9 h-full first:ml-0"
+              v-for="menuItem in menuItems"
+              key="menuItem"
+            >
+              <a href="" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -14,6 +30,14 @@
 
 <script setup>
 const company = "Careers";
+const url = "https://careers.google.com/";
+const menuItems = [
+  "Teams",
+  "Locations",
+  "Life at Bobo Corp",
+  "How we hire",
+  "Jobs",
+];
 </script>
 
 <style lang="scss" scoped></style>
--- src/tests/unit/components/MainVue.test.js
+++ src/tests/unit/components/MainVue.test.js
@@ -13,4 +13,20 @@ describe("MainVue.vue", () => {
     });
     expect(screen.getByText(/careers/i)).toBeInTheDocument();
   });
+
+  it("describe menu items for navigation", () => {
+    render(MainVue);
+
+    const navigationItems = screen.getAllByRole("listitem");
+
+    const navigationTextContent = navigationItems.map(item => item.textContent);
+
+    expect(navigationTextContent).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo Corp",
+      "How we hire",
+      "Jobs",
+    ]);
+  });
 });
